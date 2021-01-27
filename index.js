784fd72b06f548304a6809c145814599ac2ae751
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-01-27 11:01:12 +0100

    solution added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //divisibleBy3() should return if dividing a given number by 3 returns a round number: true/false
 
-function divisibleBy3() {
-// WRITE ME!
+function divisibleBy3(number) {
+if (number%3===0) {
+    return true;
 }
-
+else {
+    return false;
+}
+}
 
 module.exports = {
     divisibleBy3:divisibleBy3,
