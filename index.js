15374f441842762bbd7679d05057fed5459b0ed4
Frederiek107 <f.a.tenbroeke@gmail.com>
2021-01-27 10:43:28 +0100

    function added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function divideBy3() {
+//divisibleBy3() should return if dividing a given number by 3 returns a round number: true/false
+
+function divisibleBy3() {
 // WRITE ME!
 }
 
 
 module.exports = {
-    divideBy3:divideBy3,
+    divisibleBy3:divisibleBy3,
 }
--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
-const divideBy3 = require("./index").divideBy3;
+const divisibleBy3 = require("./index").divisibleBy3;
 
-test ("divideBy3() should return if dividing a given number by 3 returns a round number: true/false" , ()=> {
+test ("divisibleBy3() should return if dividing a given number by 3 returns a round number: true/false" , ()=> {
 //ARRANGE
 const num1=1356
 const num2=-1356
@@ -8,10 +8,10 @@ const num3=16
 const num4=0
 
 //ACT
-const result1 = divideBy3(num1);
-const result2 = divideBy3(num2);
-const result3 = divideBy3(num3);
-const result4 = divideBy3(num4);
+const result1 = divisibleBy3(num1);
+const result2 = divisibleBy3(num2);
+const result3 = divisibleBy3(num3);
+const result4 = divisibleBy3(num4);
 
 //ASSERT
 expect(result1).toBe(true);
